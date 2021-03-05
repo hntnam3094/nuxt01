da3e4e3039734515cdf1dc3c80eed92bd97c847e
[Lampart] Hoang Ngoc Thanh Nam <thanh_nam@lampart-vn.com>
2021-03-05 14:57:58 +0700

    deloy

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
-    target: 'static',
+    target: 'ssr',
     head: {
         titleTemplate: '%s - Premium Water',
         title: 'Premium Water',
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,6 @@
     <h3>Deloy downtime v1</h3>
     <h3>Deloy downtime v1 to v1.1</h3>
     <h3>Deloy downtime v1.1 to v1.2</h3>
+    <h3>Deloy downtime v1.2 to v1.3</h3>
   </div>
 </template>
