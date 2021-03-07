e3c6501bede9e8f86ec0074da8772ef6b7fee3d3
Hoang Ngoc Thanh Nam <hntnam98@gmail.com>
2021-03-07 04:07:51 -0500

    deloy lan thu n

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,5 @@
   <div>
     <h3>Deloy downtime v1</h3>
     <h3>Deloy downtime v1 to v1.1</h3>
-    <h3>Deloy downtime v1.1 to v1.2</h3>
-    <h3>Deloy downtime v1.2 to v1.3</h3>
-    <h3>Deloy downtime v1.3 to v1.4</h3>
-    <h3>Deloy downtime v1.4 to v1.5</h3>
-    <h3>Deloy downtime v1.5 to v1.6</h3>
-    <h3>Deloy downtime v1.6 to v1.7</h3>
   </div>
 </template>
