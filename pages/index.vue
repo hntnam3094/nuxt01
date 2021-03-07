f774c9a2d8ed6d9041da8d345fc5945b132afa28
Hoang Ngoc Thanh Nam <hntnam98@gmail.com>
2021-03-07 05:15:17 -0500

    deloy v1

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <h3>Deloy downtime v1</h3>
-    <h3>Deloy downtime with github v1 to v1.1</h3>
-    <h3>Deloy downtime with github v1.1 to v1.2</h3>
+    <h3>Connect to github</h3>
 
   </div>
 </template>
