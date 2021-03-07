f8f0f07f4582d4dcee06ea6e6ec27f32b834c154
Hoang Ngoc Thanh Nam <hntnam98@gmail.com>
2021-03-07 03:53:16 -0500

    deloy to see dynos

--- pages/index.vue
+++ pages/index.vue
@@ -7,5 +7,6 @@
     <h3>Deloy downtime v1.3 to v1.4</h3>
     <h3>Deloy downtime v1.4 to v1.5</h3>
     <h3>Deloy downtime v1.5 to v1.6</h3>
+    <h3>Deloy downtime v1.6 to v1.7</h3>
   </div>
 </template>
